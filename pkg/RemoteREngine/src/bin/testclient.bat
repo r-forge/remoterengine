5863543dbd614e5b3e18d441a69c9863446b3bf2
romain (R Enthusiast ) <francoisromain@free.fr>
2009-09-06 19:16:10 +0000

    removed src directory
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/remoterengine@92 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RemoteREngine/src/bin/testclient.bat
+++
@@ -1,3 +0,0 @@
-set DIR=..\..\inst
-java -cp %DIR%/java/RemoteREngine-client.jar -Djava.rmi.server.codebase=file:%DIR%\client.jar -Djava.security.policy=../../inst/policy/client.policy org.rosuda.REngine.remote.client.test.RemoteREngineTest
-
