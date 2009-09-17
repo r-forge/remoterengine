355efd2e9074727cfef405d72858a9d125341ebc
romain (R Enthusiast ) <francoisromain@free.fr>
2009-09-17 12:52:55 +0000

    added run fucntion
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/remoterengine@148 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RemoteREngine.test/R/zzz.R
+++ pkg/RemoteREngine.test/R/zzz.R
@@ -1,2 +1,24 @@
 .onAttach <- function(libname, pkgname){}
 
+run <- function(){
+	rscript( "RemoteREngine.test", "runtests" , run = TRUE, arguments = commandArgs(TRUE) )
+}
+
+rscript <- function( package, script, arguments = commandArgs(TRUE), run = TRUE, dir = "exec" ){
+	
+	Rscript <- file.path( R.home(), "bin", "Rscript" )
+	script  <- system.file( dir, script, package = package )
+	if( !file.exists( script ) ){
+		stop( sprintf( "script '%s' does not exist", script) )
+	}
+	
+	arguments <- if( length( arguments ) == 0 ) "" else paste( arguments, collapse = " " )
+	cmd <- sprintf( '"%s" "%s" %s', Rscript, script, arguments )
+	
+	if( run ){
+		system( cmd )
+	} else {
+		cat( cmd )
+	}
+}
+
