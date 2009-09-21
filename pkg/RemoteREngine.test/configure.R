ad5bedb423c5208776d78d737c283fb3effef820
romain (R Enthusiast ) <francoisromain@free.fr>
2009-09-21 14:50:29 +0000

    move some of the R logic out of the build.xml file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/remoterengine@170 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/RemoteREngine.test/configure.R
+++ pkg/RemoteREngine.test/configure.R
@@ -1,5 +1,45 @@
 #!/bin/env Rscript
 
+# {{{ :tabSize=2:indentSize=2:noTabs=false:folding=explicit:collapseFolds=1:
+# This file is part of the RemoteREngine project
+# 
+# The RemoteREngine project is free software: 
+# you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+# 
+# The RemoteREngine project is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with the RemoteREngine project. If not, see <http://www.gnu.org/licenses/>. 
+# }}}
+
+# {{{ generate.build.properties
+generate.build.properties <- function(  ){
+	R_PACKAGE_DIR <- Sys.getenv( "R_PACKAGE_DIR" ) 
+	testng.jar    <- file.path( R_PACKAGE_DIR, "java_src", "lib", "testng-5.10-jdk15.jar" )
+	client.jar    <- system.file( "java", "RemoteREngine-client.jar", package = "RemoteREngine" )
+	testcases.jar <- file.path( R_PACKAGE_DIR, "java", "RemoteREngine-test.jar" )
+	rmicodebase   <- paste( "file:/", system.file( "java", "RemoteREngine-client.jar", package = "RemoteREngine" ), sep = "" )
+	client.policy <- system.file( "policy", "client.policy", package = "RemoteREngine" ) 
+	server.policy <- system.file( "policy", "server.policy", package = "RemoteREngine" ) 
+	
+	cat( "\n\n\n# generated content below\n\n",
+		"testng.jar=", testng.jar, "\n", 
+		"client.jar=", client.jar, "\n", 
+		"testcases.jar=", testcases.jar, "\n", 
+		"rmicodebase=", rmicodebase, "\n", 
+		"client.policy=", client.policy, "\n" ,
+		"server.policy=", server.policy, "\n", 
+		file = "build.properties" ,	sep = "", 
+		append = TRUE )
+}			
+# }}}
+
 require( ant ) 
 
 rscript <- file.path( R.home(), "bin", "Rscript" )
@@ -8,6 +48,8 @@ dir.create( "inst/java" )
 setwd( "inst/java_src" )
 
 java_src.dir <- getwd()
+generate.build.properties()
+
 ant()
 setwd(root)
 
